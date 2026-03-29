110b1246b14484a620c6ab0912450cd4a273e07e
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2026-03-29 20:45:11 +0100

    Hero Section

---
+++ src/pages/public/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeroSection } from '../../../components/public/HeroSection';
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
