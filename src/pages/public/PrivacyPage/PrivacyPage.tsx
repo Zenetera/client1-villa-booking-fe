eee36a99416e36919cf782e1050422d47ed2af8f
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2026-03-31 00:19:02 +0100

    build error fix

--- src/pages/public/PrivacyPage/PrivacyPage.tsx
+++ src/pages/public/PrivacyPage/PrivacyPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './PrivacyPage.module.css';
 
 export function PrivacyPage() {
--- src/pages/public/TermsPage/TermsPage.tsx
+++ src/pages/public/TermsPage/TermsPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './TermsPage.module.css';
 
 export function TermsPage() {
